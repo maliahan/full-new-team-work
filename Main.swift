6a585d4b1bb2cb2944a385465272502608285906
maliahan <amapps1977@gmail.com>
2018-07-04 23:08:06 +0430

    first commit

---
+++ Main.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  ghost escape
+//
+//  Created by ali on 6/19/17.
+//  Copyright © 2017 amapps. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+class main: SKScene {
+    
+}
